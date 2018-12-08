6049c9d38044c804b69850406df5c73143aaf64c
11618nathan <11618nathan@gmail.com>
2018-12-08 17:00:51 +0900

    Upload

---
+++ string/string pointer length.c
@@ -0,0 +1,24 @@
+// 문자열 길이 - 포인터 사용
+
+#include <stdio.h>
+
+int StringPointer(char *s)
+{
+	int len = 0;
+	while (*s++)
+	{
+		len++;
+	}
+	return len;
+}
+
+int main()
+{
+	int len;
+	char string[100] = "";
+	
+	printf("문자열 입력: ");
+	gets(string);
+
+	printf("문자열 길이: %d \n", StringPointer(string));
+}
